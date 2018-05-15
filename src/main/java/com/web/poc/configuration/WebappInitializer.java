413bda030d2b251ea2bb1a50d580df9672ceadcd
hramteke <himanshuramteke@yahoo.com>
2018-05-15 07:13:18 -0700

    First Commit

---
+++ src/main/java/com/web/poc/configuration/WebappInitializer.java
@@ -0,0 +1,30 @@
+package com.web.poc.configuration;
+
+import javax.servlet.Filter;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class WebappInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+  
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[] { WebappConfiguration.class };
+    }
+   
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return null;
+    }
+   
+    @Override
+    protected String[] getServletMappings() {
+        return new String[] { "/" };
+    }
+     
+    @Override
+    protected Filter[] getServletFilters() {
+        Filter [] singleton = { new CORSFilter() };
+        return singleton;
+    }
+  
+}
